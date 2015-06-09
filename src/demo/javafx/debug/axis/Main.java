ce79f1b447379963f28ae5050bb37767089d11ab
Pierre Monnin <pier.monn@gmail.com>
2015-06-09 20:05:55 +0200

    [+] Adding mouse control in the demo

---
+++ src/demo/javafx/debug/axis/Main.java
@@ -0,0 +1,104 @@
+package javafx.debug.axis;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+/**
+ * Main class starting the JavaFX application and displaying the demo
+ * of the debug axis
+ * @author Pierre Monnin
+ */
+public class Main extends Application {
+    private Scene scene;
+    private Group root;
+
+    private Group cameraGroup;
+    private PerspectiveCamera camera;
+
+    private double oldMouseX;
+    private double oldMouseY;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Box
+        Sphere sphere = new Sphere(2);
+        sphere.setMaterial(new PhongMaterial(Color.RED));
+
+        // Camera
+        this.camera = new PerspectiveCamera(true);
+        this.cameraGroup = new Group();
+        this.cameraGroup.getChildren().add(this.camera);
+        this.cameraGroup.getTransforms().addAll(new Translate(0, 0, -15));
+
+        // Root node
+        this.root = new Group();
+        this.root.getChildren().add(this.cameraGroup);
+        this.root.getChildren().add(sphere);
+
+        // Scene
+        this.scene = new Scene(root, 800, 600, true, SceneAntialiasing.BALANCED);
+        this.scene.setFill(Color.WHITESMOKE);
+        this.scene.setCamera(this.camera);
+
+        // Controls for the scene
+        handleKeyboard();
+        handleMouse();
+
+        // Primary stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Demo - JavaFX debug axis");
+        primaryStage.show();
+    }
+
+    public void handleKeyboard() {
+
+    }
+
+
+    /**
+     * Handle mouse dragging to change the camera orientation
+     */
+    public void handleMouse() {
+        this.scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.isPrimaryButtonDown()) {
+                    Main.this.oldMouseX = event.getX();
+                    Main.this.oldMouseY = event.getY();
+                }
+            }
+        });
+
+        this.scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.isPrimaryButtonDown()) {
+                    double deltaX = event.getX() - Main.this.oldMouseX;
+                    double deltaY = event.getY() - Main.this.oldMouseY;
+
+                    Main.this.oldMouseX = event.getX();
+                    Main.this.oldMouseY = event.getY();
+
+
+                    Main.this.cameraGroup.getTransforms().add(new Rotate(deltaX * 0.01, Rotate.Y_AXIS));
+                    Main.this.cameraGroup.getTransforms().add(new Rotate(deltaY * 0.01, Rotate.X_AXIS));
+                }
+            }
+        });
+    }
+
+    /**
+     * Java main for running without JavaFX launcher
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/javafx/debug/axis/DebugAxis.java
@@ -0,0 +1,7 @@
+package javafx.debug.axis;
+
+/**
+ * Created by frog on 07/06/15.
+ */
+public class DebugAxis {
+}
